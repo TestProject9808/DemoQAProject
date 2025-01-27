3085cb858c619e9b4bb86293b50298459477cd35
TestProject9808 <testp9808@gmail.com>
2025-01-27 23:46:45 +0400

    TextBoxValidation

--- cypress/e2e/UI/TextBox.cy.js
+++ cypress/e2e/UI/TextBox.cy.js
@@ -3,10 +3,10 @@ describe("DemoQA - Text Box Tests", () => {
   const { testData } = Cypress.env();
 
   beforeEach(() => {
-    textBox.visit("/");
+    textBox.visit("/text-box");
   });
 
-  it("1. Verify Page Elements are Displayed", () => {
+  it("Verify Page Elements are Displayed", () => {
     cy.get(textBox.fullNameField).should("be.visible");
     cy.get(textBox.emailField).should("be.visible");
     cy.get(textBox.currentAddressField).should("be.visible");
@@ -14,26 +14,25 @@ describe("DemoQA - Text Box Tests", () => {
     cy.get(textBox.submitButton).should("be.visible");
   });
 
-  it("2. Input Valid Data and Submit", () => {
+  it("Input Valid Data and Submit", () => {
     textBox.fillForm(testData.validData);
     textBox.submitForm();
     textBox.verifyOutput(testData.validData);
   });
 
-  it("4. Validate Email Field", () => {
+  it("Validate Email Field", () => {
     testData.invalidEmails.forEach((email) => {
       cy.get(textBox.emailField).clear().type(email);
       textBox.submitForm();
       cy.get(textBox.outputEmail).should("not.exist");
     });
 
-    // Test with a valid email
     cy.get(textBox.emailField).clear().type(testData.validData.email);
     textBox.submitForm();
     cy.get(textBox.outputEmail).should("contain", testData.validData.email);
   });
 
-  it("6. Clear Field and Re-enter Data", () => {
+  it("Clear Field and Re-enter Data", () => {
     textBox.fillForm(testData.validData);
     cy.get(textBox.fullNameField).clear();
     cy.get(textBox.emailField).clear();
@@ -45,7 +44,7 @@ describe("DemoQA - Text Box Tests", () => {
     textBox.verifyOutput(testData.updatedData);
   });
 
-  it("7. Validate Special Characters in Fields", () => {
+  it("Validate Special Characters in Fields", () => {
     cy.get(textBox.fullNameField).type(testData.specialChars);
     cy.get(textBox.emailField).type(testData.validData.email);
     cy.get(textBox.currentAddressField).type(testData.specialChars);
