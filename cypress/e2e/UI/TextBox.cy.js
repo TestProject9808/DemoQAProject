225c0db459ca589c33e9e29349eff4eaa8174d62
TestProject9808 <testp9808@gmail.com>
2025-01-27 23:36:24 +0400

    TextBoxValidation

--- cypress/e2e/UI/TextBox.cy.js
+++ cypress/e2e/UI/TextBox.cy.js
@@ -1,13 +1,65 @@
-import { basePage } from "../../Pages/Base";
 import { textBox } from "../../Pages/TextBox";
+describe("DemoQA - Text Box Tests", () => {
+  const { testData } = Cypress.env();
 
-describe("Main page testing", () => {
   beforeEach(() => {
-    cy.visit("/");
-    cy.contains(basePage.NAMES.elements).click();
+    textBox.visit("/");
   });
-  it("test", () => {
-    cy.get(".header-text").should("contain", basePage.NAMES.elements);
-    cy.get("#item-0").contains("Text Box").click();
+
+  it("1. Verify Page Elements are Displayed", () => {
+    cy.get(textBox.fullNameField).should("be.visible");
+    cy.get(textBox.emailField).should("be.visible");
+    cy.get(textBox.currentAddressField).should("be.visible");
+    cy.get(textBox.permanentAddressField).should("be.visible");
+    cy.get(textBox.submitButton).should("be.visible");
+  });
+
+  it("2. Input Valid Data and Submit", () => {
+    textBox.fillForm(testData.validData);
+    textBox.submitForm();
+    textBox.verifyOutput(testData.validData);
+  });
+
+  it("4. Validate Email Field", () => {
+    testData.invalidEmails.forEach((email) => {
+      cy.get(textBox.emailField).clear().type(email);
+      textBox.submitForm();
+      cy.get(textBox.outputEmail).should("not.exist");
+    });
+
+    // Test with a valid email
+    cy.get(textBox.emailField).clear().type(testData.validData.email);
+    textBox.submitForm();
+    cy.get(textBox.outputEmail).should("contain", testData.validData.email);
+  });
+
+  it("6. Clear Field and Re-enter Data", () => {
+    textBox.fillForm(testData.validData);
+    cy.get(textBox.fullNameField).clear();
+    cy.get(textBox.emailField).clear();
+    cy.get(textBox.currentAddressField).clear();
+    cy.get(textBox.permanentAddressField).clear();
+
+    textBox.fillForm(testData.updatedData);
+    textBox.submitForm();
+    textBox.verifyOutput(testData.updatedData);
+  });
+
+  it("7. Validate Special Characters in Fields", () => {
+    cy.get(textBox.fullNameField).type(testData.specialChars);
+    cy.get(textBox.emailField).type(testData.validData.email);
+    cy.get(textBox.currentAddressField).type(testData.specialChars);
+    cy.get(textBox.permanentAddressField).type(testData.specialChars);
+
+    textBox.submitForm();
+    cy.get(textBox.outputName).should("contain", testData.specialChars);
+    cy.get(textBox.outputCurrentAddress).should(
+      "contain",
+      testData.specialChars
+    );
+    cy.get(textBox.outputPermanentAddress).should(
+      "contain",
+      testData.specialChars
+    );
   });
 });
