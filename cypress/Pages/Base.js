114f1eee3a9a63bc5c52d44bffad49e1643eb4f6
TestProject9808 <testp9808@gmail.com>
2025-01-30 19:03:01 +0400

    Change

--- cypress/Pages/Base.js
+++ cypress/Pages/Base.js
@@ -2,22 +2,46 @@
 
 class BasePage {
   NAMES = {
-    elements: "Elements",
-    forms: "Forms",
-    alertsFrameWindow: "Alerts, Frame & Windows",
-    widgets: "Widgets",
-    interactions: "Interactions",
-    bookStoreApplication: "Book Store Application",
+    messageSelectSector: "Please select an item from left to start practice.",
+    loginButton: "Login",
   };
+
+  sections = [
+    "Elements",
+    "Forms",
+    "Alerts, Frame & Windows",
+    "Widgets",
+    "Interactions",
+    "Book Store Application",
+  ];
+
+  subSectionsCount = [9, 1, 5, 9, 5, 4];
+
+  sectionURL = [
+    "elements",
+    "forms",
+    "alertsWindows",
+    "widgets",
+    "interaction",
+    "books",
+  ];
 
   LOCATORS = {
     active: "active",
     h1: "h1",
-    svg: "svg"
+    svg: "svg",
   };
+
+  getSubSection() {
+    return cy.get(".collapse.show").find("li");
+  }
 
   getHeaderText() {
     return cy.get(".header-text");
+  }
+
+  getSectorCollapse() {
+    return cy.get("div.element-list.collapse");
   }
 }
 
--- cypress/e2e/UI/CheckBox.cy.js
+++ cypress/e2e/UI/CheckBox.cy.js
@@ -4,12 +4,12 @@ import { checkBox } from "../../Pages/CheckBox";
 describe("Main page testing", () => {
   beforeEach(() => {
     cy.visit("/");
-    cy.contains(basePage.NAMES.elements).should("be.visible");
-    cy.contains(basePage.NAMES.elements).click();
+    cy.contains(basePage.sections[0]).should("be.visible");
+    cy.contains(basePage.sections[0]).click();
   });
 
   it("Verifies check boxes button functionality and Page", () => {
-    basePage.getHeaderText().should("contain", basePage.NAMES.elements);
+    basePage.getHeaderText().should("contain", basePage.sections[0]);
     cy.contains(checkBox.NAMES.checkBox).should("be.visible");
     checkBox.getCheckboxID().should("not.have.class", basePage.LOCATORS.active);
     checkBox.getCheckboxID().contains(checkBox.NAMES.checkBox).click();
